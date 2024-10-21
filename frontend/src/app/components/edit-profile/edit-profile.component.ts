babc4f2b165779eaf7b1470f687b4390836c899c
jceso <01gabrielecesali@gmail.com>
2024-10-21 16:44:01 +0200

    fixed background and editing page

--- frontend/src/app/components/edit-profile/edit-profile.component.ts
+++ frontend/src/app/components/edit-profile/edit-profile.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.css'
 })
-export class EditProfileComponent {
+export class EditProfileComponent implements OnInit {
+  
+  constructor(private auth: AuthService) { }
+
+  userInfo: User = {
+    _id: '',
+    name: '',
+    email: '',
+    phonenumber: '',
+    password: '',
+    points: 0,
+    role: 'USER'
+  };
+
+  ngOnInit(): void {
+    this.getInfo();
+  }
+
+  getInfo(): void {
+    this.auth.getInfo().subscribe({
+      next: (data: User) => {
+        this.userInfo = data;
+      }, error: (err: any) => { }
+    });
+  }
 
 }
